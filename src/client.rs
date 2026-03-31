2971cdca516c80bcf4c77d215fe2e117c5995c32
a-rvid <git@rvid.eu>
2026-03-31 09:24:06 +0200

    yaya

--- src/client.rs
+++ src/client.rs
@@ -15,22 +15,22 @@ use ratatui::{DefaultTerminal, Frame};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let stream = UnixStream::connect("/tmp/tuxmux.sock").await?;
-    let (mut r, mut w) = stream.into_split();
+    // let stream = UnixStream::connect("/tmp/tuxmux.sock").await?;
+    // let (mut r, mut w) = stream.into_split();
 
     // w.write_all to execute
     // socket -> stdout
     // let tx = Arc::new(Mutex::new(w));
 
-    let mut stdout = io::stdout();
-    let mut buf = [0u8; 1024];
-    tokio::spawn(async move {
-        loop {
-            let n = r.read(&mut buf).await.unwrap();
-            if n == 0 { break; }
-            let s = String::from_utf8_lossy(&buf[..n]);
-        }
-    });
+    // let mut stdout = io::stdout();
+    // let mut buf = [0u8; 1024];
+    // tokio::spawn(async move {
+    //     loop {
+    //         let n = r.read(&mut buf).await.unwrap();
+    //         if n == 0 { break; }
+    //         let s = String::from_utf8_lossy(&buf[..n]);
+    //     }
+    // });
 
     color_eyre::install()?;
     ratatui::run(|terminal| App::new().run(terminal))
